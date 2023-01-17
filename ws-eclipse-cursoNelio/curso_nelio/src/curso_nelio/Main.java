df3576de437518fe04adb84c0098aff8c790ef6e
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-17 18:53:08 -0300

    Locale US e casas decimais pós virgula

--- ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
@@ -1,10 +1,36 @@
 package curso_nelio;
 
+import java.util.Locale;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-	System.out.println("Hi");
+	//Saida de dados
+		
+	int x = 36;
+	double y = 10.35784;
+	
+	System.out.println("Teste saida de dados");
+	System.out.print("Valor de x: ");
+	System.out.println(x);
+	
+	System.out.print("Valor de y: ");
+	System.out.println(y);
+	
+	System.out.print("Valor de y com máscara para duas casas decimais: ");
+	System.out.printf("%.2f%n", y);
+	
+	System.out.print("Valor de y com máscara para quatro casas decimais: ");
+	System.out.printf("%.4f%n", y);
+	
+	//Os valores saem pelo padrão de lingua do PC, caso eu queira mudar a saida 
+	//dados para . por exemplo uso comando Locate a seguir...
+	
+	Locale.setDefault(Locale.US);
+	System.out.print("Valor de y com máscara para quatro casas decimais: ");
+	System.out.printf("%.4f%n", y);
+	
 		
 	}
 
