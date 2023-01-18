26566cda6026c4926144b385f5d2e15683aa7a65
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-18 11:56:05 -0300

    Exemplo de Casting

--- ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
@@ -1,33 +1,23 @@
 package curso_nelio;
 
-import java.util.Locale;
-
 public class Main {
 
 	public static void main(String[] args) {
+	//Casting
 		
-		String product1 = "Computer";
-		String product2 = "Office desk";
-		
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
-		
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
-		
-		System.out.println("Products:");
-		System.out.printf("%s, which price is $ %.2f", product1 , price1);
-		System.out.printf("%n%s, which price is $ %.2f", product2, price2);
-		
-		System.out.printf("%n%nRecord: %d years old, code %d and gender: %s" , age, code, gender);
-		
-		System.out.printf("%n%nMeasue with eight decimal places: %.8f" , measure);
-		System.out.printf("%nRouded (three decimal places): %.3f" , measure);
-		
-		Locale.setDefault(Locale.US);
-		System.out.printf("%nUS decimal point %.3f" , measure);
+	int a = 5;
+	int b = 2;
+	
+	double resultado;
+	
+	resultado = a / b;
+	System.out.println("Sem casting...");
+	System.out.println(resultado);
+	
+	//Colocar na frente entre parentêses o tipo da variável
+	resultado = (double) a / b;
+	System.out.println("Com casting...");
+	System.out.println(resultado);	
 		
 	}
 
