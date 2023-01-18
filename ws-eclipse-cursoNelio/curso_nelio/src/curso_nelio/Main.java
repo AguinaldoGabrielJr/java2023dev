7762b95f00c12fb1333be1d467d795479b51feb3
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-18 12:14:04 -0300

    Entrada de dados 1

--- ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
@@ -1,24 +1,41 @@
 package curso_nelio;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-	//Casting
+		// Entrada de dados
+
+		Scanner sc = new Scanner(System.in);
+
+		String x;
+		int y;
+		double f;
+		char c, d;
 		
-	int a = 5;
-	int b = 2;
-	
-	double resultado;
-	
-	resultado = a / b;
-	System.out.println("Sem casting...");
-	System.out.println(resultado);
-	
-	//Colocar na frente entre parentêses o tipo da variável
-	resultado = (double) a / b;
-	System.out.println("Com casting...");
-	System.out.println(resultado);	
+		System.out.println("Por favor digite algo: ");
+		x = sc.next();
 		
+		System.out.println("Por favor digite um número inteiro: ");
+		y = sc.nextInt();
+		
+		System.out.println("Por favor digite um número: ");
+		f = sc.nextDouble();
+		
+		System.out.println("Por favor digite algo(Irá aparecer somente a posição zero: ");
+		c = sc.next().charAt(0);
+		
+		System.out.println("Por favor digite algo(Irá aparecer somente a posição hum: ");
+		d = sc.next().charAt(1);
+
+		System.out.println("Você digitou: " + x);
+		System.out.println("Você digitou: " + y);
+		System.out.println("Você digitou: " + f);
+		System.out.println("Você digitou: " + c);
+		System.out.println("Você digitou: " + d);
+		
+		sc.close();
 	}
 
 }
