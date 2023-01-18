07b9e7fb2bba1a4e734b73641d694dfd1186af27
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-18 15:10:26 -0300

    Algumas fuñções matemáticas

--- ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/curso_nelio/Main.java
@@ -5,29 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// Entrada de dados, ler texto até quebra de linha(Apertar Enter)
+		// Funções matemáticas
 
-		Scanner sc = new Scanner(System.in);
-		
-		int x;
-		String s1, s2,s3;
-		
-		//Tomar cuidado quando colocar algum next diferente de nextLine(), pois ele irá consumir o mesmo.
-		//Para burlar isso posso faço um nextLine() extra... Como na linha 19.
-		
-		x = sc.nextInt();
-		sc.nextLine();
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
-		
-		System.out.println("Resultados:\n");
-		System.out.println(x);
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		
-		sc.close();
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadrado = " + C);
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
 	}
 
 }
