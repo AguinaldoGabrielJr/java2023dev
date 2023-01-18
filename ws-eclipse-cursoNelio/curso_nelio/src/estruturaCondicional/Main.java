28855692622a0dcc0f53e3b582e58546da1ee47c
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-18 18:31:26 -0300

    If else

--- ws-eclipse-cursoNelio/curso_nelio/src/estruturaCondicional/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/estruturaCondicional/Main.java
@@ -1,11 +1,48 @@
 package estruturaCondicional;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-	
-	//Operadores:&&(e), ||(ou), !(não)
-	
-	}
+		// Estrutura condicional if else
 
+		// Simples
+		System.out.println("Estrutura simples, se for verdadeira executa senão não faz nada...\n");
+
+		int hora;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Que horas são?");
+		hora = sc.nextInt();
+
+		System.out.println("======================SIMPLES=========================");
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		}
+
+		// Composta
+		System.out.println("Estrutura composta, se for verdadeira executa o se, senão o outro...\n");
+
+		System.out.println("======================COMPOSTA=========================");
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else {
+			System.out.println("Boa tarde");
+		}
+
+		// Encadeada
+		System.out.println("Estrutura composta encadeada, se for verdadeira executa o se, senão o outro...\n");
+
+		System.out.println("======================ENCADEADA=========================");
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else if (hora < 18) {
+			System.out.println("Boa tarde");
+		} else {
+			System.out.println("Boa noite");
+		}
+
+		sc.close();
+	}
 }
