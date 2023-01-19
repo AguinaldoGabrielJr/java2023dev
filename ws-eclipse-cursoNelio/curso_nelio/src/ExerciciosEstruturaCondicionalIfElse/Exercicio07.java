1b5c51ee8e4c710a2f7124f7fb07126d8fb5024a
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-19 14:48:44 -0300

    Exercicio 7

---
+++ ws-eclipse-cursoNelio/curso_nelio/src/ExerciciosEstruturaCondicionalIfElse/Exercicio07.java
@@ -0,0 +1,40 @@
+package ExerciciosEstruturaCondicionalIfElse;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		float x, y;
+
+		System.out.println("Por favor insira o eixo X: ");
+		x = sc.nextFloat();
+
+		System.out.println("Por favor insira o eixo Y: ");
+		y = sc.nextFloat();
+
+		if (x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		} else if (x == 0.0) {
+			System.out.println("Eixo Y");
+		} else if (y == 0.0) {
+			System.out.println("Eixo X");
+		} else if (x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		} else if (x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		} else if (x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		} else if (x > 0.0 && y < 0.0) {
+			System.out.println("Q4");
+		}
+		sc.close();
+
+	}
+
+}
