4f7b570b516a6b4f1714f31a90eef61531e29d7f
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-19 16:15:05 -0300

    Switch case

--- ws-eclipse-cursoNelio/curso_nelio/src/estruturaSequencial/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/estruturaSequencial/Main.java
@@ -1,30 +1,55 @@
 package estruturaSequencial;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// Funções matemáticas
+		// Switch case
+		//Domingo dia 1 até segunda dia 7
 
-		double x = 3.0;
-		double y = 4.0;
-		double z = -5.0;
-		double A, B, C;
-		A = Math.sqrt(x);
-		B = Math.sqrt(y);
-		C = Math.sqrt(25.0);
-		System.out.println("Raiz quadrada de " + x + " = " + A);
-		System.out.println("Raiz quadrada de " + y + " = " + B);
-		System.out.println("Raiz quadrada de 25 = " + C);
-		A = Math.pow(x, y);
-		B = Math.pow(x, 2.0);
-		C = Math.pow(5.0, 2.0);
-		System.out.println(x + " elevado a " + y + " = " + A);
-		System.out.println(x + " elevado ao quadrado = " + B);
-		System.out.println("5 elevado ao quadrado = " + C);
-		A = Math.abs(y);
-		B = Math.abs(z);
-		System.out.println("Valor absoluto de " + y + " = " + A);
-		System.out.println("Valor absoluto de " + z + " = " + B);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira o dia da semana...");
+		
+		int x = sc.nextInt();
+		String dia;
+		
+		switch (x) {
+		case 1: {
+			dia = "domingo";
+			break;
+		}
+		case 2: {
+			dia = "segunda";
+			break;
+		}
+		case 3: {
+			dia = "terça";
+			break;
+		}
+		case 4: {
+			dia = "quarta";
+			break;
+		}
+		case 5: {
+			dia = "quinta";
+			break;
+		}
+		case 6: {
+			dia = "sexta";
+			break;
+		}
+		case 7: {
+			dia = "sabado";
+			break;
+		}
+		default:
+			dia = "Valor invalido";
+		}
+		
+		System.out.println("Dia da semana é " + dia);
+		sc.close();
+
 	}
 
 }
