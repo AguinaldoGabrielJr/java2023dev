0200d8b8235e24079affc32319e7b123c8a1e2fa
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-20 15:23:47 -0300

    Exercicio 3 e 4 for

---
+++ ws-eclipse-cursoNelio/curso_nelio/src/ExerciciosEstruturaCondicionalFor/Exercicio03.java
@@ -0,0 +1,32 @@
+package ExerciciosEstruturaCondicionalFor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio03 {
+	//Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+	//de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+	//conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+	//peso 5.
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Número de repetições: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			float a, b ,c, media;
+			a = sc.nextFloat();
+			b = sc.nextFloat();
+			c = sc.nextFloat();
+			
+			media = ((a * 2)+(b *3)+(c* 5)) / 10;
+			System.out.printf("%.1f%n", media);
+		}
+		
+		sc.close();
+
+	}
+}
---
+++ ws-eclipse-cursoNelio/curso_nelio/src/ExerciciosEstruturaCondicionalFor/Exercicio04.java
@@ -0,0 +1,36 @@
+package ExerciciosEstruturaCondicionalFor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio04 {
+	//Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+	//segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			
+			float a = sc.nextInt();
+			float b = sc.nextInt();
+			
+			float divisao;
+			
+			divisao = a / b;
+
+			if (b == 0) {
+				System.err.println("divisão impossivel");
+			}else {
+				System.out.printf("%.1f",divisao);
+			}
+			
+		}
+		
+		sc.close();
+
+	}
+}
