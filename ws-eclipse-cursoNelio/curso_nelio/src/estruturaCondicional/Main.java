b87f9ea402f4c4cebc1b5603b0bcb3efe8ed6c48
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-20 14:03:31 -0300

    For progressivo/regressivo

--- ws-eclipse-cursoNelio/curso_nelio/src/estruturaCondicional/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/estruturaCondicional/Main.java
@@ -1,29 +1,20 @@
 package estruturaCondicional;
 
-import java.util.Scanner;
-
 public class Main {
-	// Utilização do for
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Insira a quantidade de repetições:");
-		int quantidade = sc.nextInt();
-		int soma = 0;
-		;
-
-		for (int i = 0; i < quantidade; i++) {
-			System.out.println("Por favor insira um número: ");
-			int x = sc.nextInt();
-			soma = soma + x;
+		// Utilização do for
+	
+		public static void main(String[] args) {
+			
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Valor de i: " + i);
+			}
+			
+			for (int i = 4; i >= 0; i--) {
+				System.out.println("Valor de i: " + i);
+			}
 		}
-		System.out.println(soma);
-
-		for (int i = 0; i < 1005; i++) {
-			System.out.println("Valor de i: " + 1);
-		}
-		 sc.close();
-	}
-
 }
+
+
+		
+
