33e7c7dde2824daecdffc811c1b7cc83228f5217
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-22 14:44:52 -0300

    Exercicio1 POO

---
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/Exercicios/Rectangle.java
@@ -0,0 +1,29 @@
+package poo.Exercicios;
+
+public class Rectangle {
+
+	double width;
+	double height;
+
+	public double area() {
+		double area = width * height;
+		return area;
+	}
+
+	public double perimeter() { //
+		double perimetro = 2 * (width + height);
+		return perimetro;
+	}
+
+	public double diagonal() {
+		double diagonal = Math.sqrt(width * width + height * height);
+		return diagonal;
+	}
+
+	public String toString() {
+		return "AREA = " + String.format("%.2f%n", area()) + 
+				"PERIMETER = " + String.format("%.2f%n", perimeter()) + 
+				"DIAGONAL = " + String.format("%.2f%n", diagonal());
+	}
+
+}
---
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/Exercicios/mainExercicio01.java
@@ -0,0 +1,24 @@
+package poo.Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class mainExercicio01 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rectangle = new Rectangle();
+		
+		System.out.println("Enter rectangle width and height:");
+		rectangle.height = sc.nextDouble();
+		rectangle.width = sc.nextDouble();
+		
+		System.out.println(rectangle);
+		System.out.println(rectangle.toString());
+		sc.close();
+	}
+
+}
