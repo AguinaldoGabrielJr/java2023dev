8fb515abf466fd8cbeb5193219347889295270c5
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-22 11:04:44 -0300

    POO MÉTODOS: para reaproveitamento e delegação

--- ws-eclipse-cursoNelio/curso_nelio/src/poo/entities/Triangle.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/entities/Triangle.java
@@ -7,5 +7,12 @@ public class Triangle {
 	Scanner sc = new Scanner(System.in);
 
 	double a, b, c;
+	
+	public double area () {
+		double p = (a + b + c) / 2.0;
+		double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return area;
+		
+	}
 
 }
