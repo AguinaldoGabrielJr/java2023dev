bf52c6d73ebd352873379d1cd9d239e126d66429
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-23 13:26:56 -0300

    Modificadores de acesso

--- ws-eclipse-cursoNelio/curso_nelio/src/construtoresSobrecargaEncapsulamento/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/construtoresSobrecargaEncapsulamento/Main.java
@@ -7,6 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		/*Modificadores de acesso
+		• https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html
+		• private: o membro só pode ser acessado na própria classe
+		• (nada): o membro só pode ser acessado nas classes do mesmo pacote
+		• protected: o membro só pode ser acessado no mesmo pacote, bem como em
+		subclasses de pacotes diferentes
+		• public: o membro é acessado por todas classes (ao menos que ele*/
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
