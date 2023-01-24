d90c52ff5363ba877d21be3927c72cb517fdf3da
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-24 11:35:30 -0300

    Exercicio 3

---
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/listas/exerciciosVetor/Exercicio04.java
@@ -0,0 +1,43 @@
+package poo.listas.exerciciosVetor;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos número voçê vai digitar? ");
+		int n = sc.nextInt();
+		int[] vetor = new int[n];
+		int totalPar = 0;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite um número: ");
+			vetor[i] = sc.nextInt();
+			
+			if (vetor[i] % 2 == 0) {
+				totalPar ++;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("NÚMEROS PARES: ");
+		
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] % 2 == 0) {
+				System.out.print(vetor[i]);
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("QUANTIDADE DE PARES = " + totalPar);
+		sc.close();
+	}
+
+}
