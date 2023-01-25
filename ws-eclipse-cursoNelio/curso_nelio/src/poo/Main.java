9c400d5a5dd55e0772962fa9360a8a47e6228dc3
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-25 08:55:08 -0300

    For each

--- ws-eclipse-cursoNelio/curso_nelio/src/poo/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/Main.java
@@ -1,32 +1,30 @@
 package poo;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		String[] vetor = new String[] { "Maria", "Bob", "João" };
+
+		System.out.println("FOR PADRÂO");
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println(vetor[i]);
+		}
 		
-		//Boxing
-		int x = 20;
-		Object obj = x; 
-		System.out.println(obj);
-	
-		//Unboxing
-		int y = (int) obj;
-		System.out.println(y);
-		
-		/*Classes equivalentes aos tipos primitivos, elas podem receber valor null diferente das primitivas
-		Uso para inserir dados em banco, pois as vezes na regra de negocio eles podem ficar vazios.
-		Toda classe java é filha de um Object e toda classe númerica é filha de Number
-		
-	    Boolean = boolean
-	    Character = char
-	    Byte = byte
-	    Short = short
-	    Integer = int
-	    Long = long
-	    Float float
-	    Double double    
-		
+		/* Sintaxe
+		for (Tipo apelido : colecao ){ Exemplo abaixo
+			<Comando 1>				   Tipo = String
+			<Comando 2>				   apelido = obj
+			<Comando n> 			   colecao = vetor
+		}
 		*/
+		System.out.println("--------------------------------");
+		System.out.println("FOR EACH");
+		for (String obj : vetor) {
+			System.out.println(obj);
+		}
 	}
-	
+
 }
