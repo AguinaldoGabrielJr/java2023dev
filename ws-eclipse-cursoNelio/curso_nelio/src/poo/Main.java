32457a764881039c3f945fefdae011f2cd59f681
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-01-25 08:47:04 -0300

    Classes Wrapper

--- ws-eclipse-cursoNelio/curso_nelio/src/poo/Main.java
+++ ws-eclipse-cursoNelio/curso_nelio/src/poo/Main.java
@@ -12,6 +12,21 @@ public class Main {
 		//Unboxing
 		int y = (int) obj;
 		System.out.println(y);
+		
+		/*Classes equivalentes aos tipos primitivos, elas podem receber valor null diferente das primitivas
+		Uso para inserir dados em banco, pois as vezes na regra de negocio eles podem ficar vazios.
+		Toda classe java é filha de um Object e toda classe númerica é filha de Number
+		
+	    Boolean = boolean
+	    Character = char
+	    Byte = byte
+	    Short = short
+	    Integer = int
+	    Long = long
+	    Float float
+	    Double double    
+		
+		*/
 	}
 	
 }
